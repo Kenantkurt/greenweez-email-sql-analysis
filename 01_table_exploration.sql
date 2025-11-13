283901a46a79ab52370287ec1435a8f852811942
Kenan Kurt <kenantkurt@gmail.com>
2025-11-13 19:51:21 +0100

    Add SQL queries to explore gwz_mail table

---
+++ 01_table_exploration.sql
@@ -0,0 +1,8 @@
+-- Explore the full structure of the gwz_mail table
+SELECT
+  *
+FROM `course14.gwz_mail`;
+
+-- Check distinct journey_id values (acts as a primary key indicator)
+SELECT DISTINCT journey_id
+FROM `course14.gwz_mail`;
